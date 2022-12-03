b37f382f542b4abb7f94d7e5c335349af9114a0e
Martin Riesz <riesz.martin@gmail.com>
2022-12-03 16:31:06 +0100

    Use python list to determine which piece to show

--- chess_game.py
+++ chess_game.py
@@ -3,6 +3,10 @@ import pygame
 
 PIECE_SIZE = 100
 
+def draw_piece(x, y, rect):
+    screen.blit(image, (PIECE_SIZE * x, PIECE_SIZE * y), rect)
+
+
 if __name__ == '__main__':
     pygame.init()
     clock = pygame.time.Clock()
@@ -21,6 +25,7 @@ if __name__ == '__main__':
                 sys.exit()
         screen.fill((225, 225, 225))
 
+        empty = pygame.Rect((0, 0, 0, 0))
         rect_rook_black = pygame.Rect((PIECE_SIZE * 2, 0, PIECE_SIZE, PIECE_SIZE))
         rect_knight_black = pygame.Rect((PIECE_SIZE * 4, 0, PIECE_SIZE, PIECE_SIZE))
         rect_king_black = pygame.Rect((0, 0, PIECE_SIZE, PIECE_SIZE))
@@ -35,29 +40,56 @@ if __name__ == '__main__':
         rect_bishop_white = pygame.Rect((PIECE_SIZE * 3, PIECE_SIZE, PIECE_SIZE, PIECE_SIZE))
         rect_pawn_white = pygame.Rect((PIECE_SIZE * 5, PIECE_SIZE, PIECE_SIZE, PIECE_SIZE))
 
-        screen.blit(image, (PIECE_SIZE * 0, 0), rect_rook_black)
-        screen.blit(image, (PIECE_SIZE * 1, 0), rect_knight_black)
-        screen.blit(image, (PIECE_SIZE * 2, 0), rect_bishop_black)
-        screen.blit(image, (PIECE_SIZE * 3, 0), rect_queen_black)
-        screen.blit(image, (PIECE_SIZE * 4, 0), rect_king_black)
-        screen.blit(image, (PIECE_SIZE * 5, 0), rect_bishop_black)
-        screen.blit(image, (PIECE_SIZE * 6, 0), rect_knight_black)
-        screen.blit(image, (PIECE_SIZE * 7, 0), rect_rook_black)
-        
-        for i in range(8):
-            screen.blit(image, (PIECE_SIZE * i, PIECE_SIZE), rect_pawn_black)
+        pieces = [
+            rect_rook_black,
+            rect_knight_black,
+            rect_bishop_black,
+            rect_queen_black,
+            rect_king_black,
+            rect_bishop_black,
+            rect_knight_black,
+            rect_rook_black,
 
-        for i in range(8):
-            screen.blit(image, (PIECE_SIZE * i, PIECE_SIZE*6), rect_pawn_white)
+            rect_pawn_black,
+            rect_pawn_black,
+            rect_pawn_black,
+            rect_pawn_black,
+            rect_pawn_black,
+            rect_pawn_black,
+            rect_pawn_black,
+            rect_pawn_black,
 
-        screen.blit(image, (PIECE_SIZE * 0, PIECE_SIZE*7), rect_rook_white)
-        screen.blit(image, (PIECE_SIZE * 1, PIECE_SIZE*7), rect_knight_white)
-        screen.blit(image, (PIECE_SIZE * 2, PIECE_SIZE*7), rect_bishop_white)
-        screen.blit(image, (PIECE_SIZE * 3, PIECE_SIZE*7), rect_queen_white)
-        screen.blit(image, (PIECE_SIZE * 4, PIECE_SIZE*7), rect_king_white)
-        screen.blit(image, (PIECE_SIZE * 5, PIECE_SIZE*7), rect_bishop_white)
-        screen.blit(image, (PIECE_SIZE * 6, PIECE_SIZE*7), rect_knight_white)
-        screen.blit(image, (PIECE_SIZE * 7, PIECE_SIZE*7), rect_rook_white)
+            empty, empty, empty, empty, empty, empty, empty, empty,
+            empty, empty, empty, empty, empty, empty, empty, empty,
+            empty, empty, empty, empty, empty, empty, empty, empty,
+            empty, empty, empty, empty, empty, empty, empty, empty,
+
+
+            rect_pawn_white,
+            rect_pawn_white,
+            rect_pawn_white,
+            rect_pawn_white,
+            rect_pawn_white,
+            rect_pawn_white,
+            rect_pawn_white,
+            rect_pawn_white,
+
+            rect_rook_white,
+            rect_knight_white,
+            rect_bishop_white,
+            rect_queen_white,
+            rect_king_white,
+            rect_bishop_white,
+            rect_knight_white,
+            rect_rook_white,
+        ]
+
+        # for x in range(8):
+        #     for y in range(8):
+        #         draw_piece(x, y, pieces[y * 8 + x])
+
+        for i in range(64):
+            draw_piece(i % 8, i // 8, pieces[i])
 
         pygame.display.flip()
         clock.tick(60)
